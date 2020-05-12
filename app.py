02d5b315f7eac778b444d3e998cca2e72d6149c7
Youssef Selkani <ynakles@gmail.com>
2020-05-12 03:26:45 +0000

    firebase push ok

--- app.py
+++ app.py
@@ -3,12 +3,33 @@
 
 import os
 from flask import Flask, render_template, url_for, json, jsonify
+app = Flask(__name__)
 import pyrebase
 
-app = Flask(__name__)
+
+config = {
+    "apiKey": "AIzaSyA4SoLI5vW5U2q6AtKAX1XXZYEhyH2qSJE",
+    "authDomain": "willapp-3ac45.firebaseapp.com",
+    "databaseURL": "https://willapp-3ac45.firebaseio.com",
+    "projectId": "willapp-3ac45",
+    "storageBucket": "willapp-3ac45.appspot.com",
+    "messagingSenderId": "712668492803",
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/firebase')
+def new_event():
+    db.child("events").push('new_event')
+    return render_template('test.html')
+
 
 @app.route('/api/v1')
 def main():
@@ -17,4 +38,6 @@ def main():
     data = json.load(open(json_url))
     return jsonify(data)
 
-if __name__ == '__main__': app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
